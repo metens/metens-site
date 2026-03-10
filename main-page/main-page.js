219a7c187352b62d1effa58c2e1ca219f7d29def
metens <nathanmetens1@gmail.com>
2026-03-10 15:31:40 -0700

    Updating home page and navbar

--- main-page/main-page.js
+++ main-page/main-page.js
@@ -1,24 +1,3 @@
 $(document).ready(() => {
-    // Load the navbar:
-    $(() => {
-        $(".navbar").load("../navbar/navbar.html");
-    });
-    $("#title").append( // Create page title using JQuery
-        $("<h1>").attr("class", "name")
-        .attr("id", "first-name")
-        .text("Nathan"),
-        $("<h1>").attr("class", "name")
-        .attr("id", "last-name")
-        .text("Metens")
-    ),
-    $("#copyright").append( // Create copyright in footer element
-        $("<div>").attr("id", "copy")
-        .text("\u00A9 2021 nmetens")
-        .css({
-            textAlign: "center",
-            color: "whitesmoke",
-            position: "relative",
-            marginBottom: "30px",
-        })
-    );
+    $(".navbar").load("../navbar/navbar.html");
 });
