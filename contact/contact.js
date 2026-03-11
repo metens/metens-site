9558bb878a6e0a7d3ecaac464e1cb62101ba9672
metens <nathanmetens1@gmail.com>
2026-03-10 18:30:31 -0700

    Updating contact page

--- contact/contact.js
+++ contact/contact.js
@@ -1,18 +1,5 @@
-
 $(document).ready(() => {
-    // Load the navbar 
-    $(document).ready(() => {
-        $(() => {
-            $(".navbar").load("../navbar/navbar.html");
-        });
+    $(() => {
+        $(".navbar").load("../navbar/navbar.html");
     });
-    $("#copyright").append( // Create the copyright in the footer element
-        $("<div>").text("\u00A9 2021 nmetens")
-        .css({
-            textAlign: "center",
-            color: "whitesmoke",
-            position: "relative",
-            marginBottom: "30px"
-        })
-    );
 });
